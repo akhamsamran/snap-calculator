7009ed83bd0e339f2cae0c8fad2d3465c0bc0586
Anna Khamsamran <akamsamran1@cnm.edu>
2018-01-29 08:31:47 -0700

    wrote it in javascript

---
+++ calculator.js
@@ -0,0 +1,14 @@
+let x = 0;
+let y = 0;
+let calculateTypes = '';
+let result = 0;
+
+if (result = x + y) {
+	console.log(x+y);
+}else if (result = x - y) {
+	console.log(x-y);
+}else if (result = x * y) {
+	console.log(x * y);
+}else (result = x / y){
+	console.log(x / y);
+}
